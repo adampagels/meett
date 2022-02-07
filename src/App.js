23768f63dda939efc0815a6af0a3081145e8a16c
Adam Pagels <pagels@hotmail.ca>
2022-02-07 12:58:11 +0100

    t

--- src/App.js
+++ src/App.js
@@ -9,51 +9,56 @@ import { extractLocations, getEvents } from './api';
 
 
 class App extends Component {
-  state = {
-    events: [],
-    locations: [],
-    numberOfEvents: 32
-  };
-
-  componentDidMount() {
-    this.mounted = true;
-    getEvents().then((events) => {
-      if (this.mounted) {
-        this.setState({
-          events: events.slice(0, this.state.numberOfEvents),
-          locations: extractLocations(events)
-        });
-      }
-    });
-  }
-
-  componentWillUnmount() {
-    this.mounted = false;
-  }
-
   render() {
     return (
-      <div className="App">
-        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
-        <EventList events={this.state.events} />
-        <NumberOfEvents numberOfEvents={this.state.numberOfEvents} />
-      </div>
-    );
+      <h1>HELLO</h1>
+    )
   }
+  // state = {
+  //   events: [],
+  //   locations: [],
+  //   numberOfEvents: 32
+  // };
 
-  updateEvents = (location) => {
-    getEvents().then((events) => {
-      const locationEvents = (location === 'all') ?
-        events :
-        events.filter((event) => event.location === location);
-      if (this.mounted) {
-        this.setState({
-          events: locationEvents.slice(0, this.state.numberOfEvents),
-          currentLocation: location,
-        });
-      }
-    });
-  }
+  // componentDidMount() {
+  //   this.mounted = true;
+  //   getEvents().then((events) => {
+  //     if (this.mounted) {
+  //       this.setState({
+  //         events: events.slice(0, this.state.numberOfEvents),
+  //         locations: extractLocations(events)
+  //       });
+  //     }
+  //   });
+  // }
+
+  // componentWillUnmount() {
+  //   this.mounted = false;
+  // }
+
+  // render() {
+  //   return (
+  //     <div className="App">
+  //       <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
+  //       <EventList events={this.state.events} />
+  //       <NumberOfEvents numberOfEvents={this.state.numberOfEvents} />
+  //     </div>
+  //   );
+  // }
+
+  // updateEvents = (location) => {
+  //   getEvents().then((events) => {
+  //     const locationEvents = (location === 'all') ?
+  //       events :
+  //       events.filter((event) => event.location === location);
+  //     if (this.mounted) {
+  //       this.setState({
+  //         events: locationEvents.slice(0, this.state.numberOfEvents),
+  //         currentLocation: location,
+  //       });
+  //     }
+  //   });
+  // }
 }
 
 export default App;
